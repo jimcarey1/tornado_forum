cd140bbc1fe2f318a4d44960f21e4d9672439e56
jimcarey1 <viswanadhlingareddy@gmail.com>
2025-08-23 15:33:33 +0530

    Added javascript function to create a nested comment.

--- tornado_forum/static/js/createComment.js
+++ tornado_forum/static/js/createComment.js
@@ -1,5 +1,6 @@
 //Only managing one CKEditor instance for all replies.
 let ckEditorInstance = null
+const url = document.getElementById('thread-reply').dataset.url
 
 const replyButton = document.querySelector(".topic-meta .fa-reply")
 const createCKEditorInstance = async (event)=>{
@@ -25,7 +26,6 @@ replyButton.onclick = createCKEditorInstance;
 const createTopicReplyButton = document.querySelector("#thread-reply .handle-comment .reply-comment")
 const createParentComment = async (event)=>{
     const topicReplyElement = document.getElementById("thread-reply")
-    const url = topicReplyElement.dataset.url
     const data = ckEditorInstance.getData();
     if(data){
         const response = await fetch(url, {
@@ -57,6 +57,7 @@ const createCKEditorInstanceForCommentReply = async (event) =>{
         try{
             if(editorElement){
                 ckEditorInstance = await ClassicEditor.create(editorElement)
+                console.log(ckEditorInstance)
             }else{
                 console.log('editorElement not found.')
             }
@@ -70,3 +71,35 @@ commentReplyButtons.forEach((button)=>{
 })
 
 
+const createChildrenCommentButtons = document.querySelectorAll('.reply-comment')
+const createChildrenComment = async(event)=>{
+    const commentId = event.target.dataset.commentId
+    const commentReplyContainer = document.getElementById(`comment-reply-${commentId}`)
+    const content = ckEditorInstance.getData()
+    if(content){
+        try{
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': getCookie('_xsrf')
+                },
+                body: JSON.stringify({'content': content, 'parent_id':commentId})
+            })
+            if(response){
+                const data = await response.json()
+                console.log(data)
+                ckEditorInstance = null
+                commentReplyContainer.style.direction = 'none'
+            }
+        }catch(error){
+            console.log('An error occured:', error)
+        }
+    }else{
+        console.log('There is no content.')
+    }
+
+}
+createChildrenCommentButtons.forEach((button)=>{
+    button.addEventListener('click', createChildrenComment)
+})
