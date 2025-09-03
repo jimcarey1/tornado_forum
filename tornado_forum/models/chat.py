43cfcc2dc5d68c8c7683678ae20a1442484a548a
jimcarey1 <viswanadhlingareddy@gmail.com>
2025-09-03 11:46:32 +0530

    created and ran alembic migrations on chat models.

--- tornado_forum/models/chat.py
+++ tornado_forum/models/chat.py
@@ -9,17 +9,18 @@ class Room(Base):
     __tablename__ = 'chat_rooms'
 
     id:Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[Text] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(unique=True)
     created_on = mapped_column(DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
-        return f'Room(id = {self.id},name = {self.name})'
+        return f'Room(id = {self.id}, name = {self.name})'
 
 class Message(Base):
     __tablename__ = 'messages'
 
     id:Mapped[int] = mapped_column(primary_key=True)
-    data:Mapped[String] = mapped_column()
+    data:Mapped[str] = mapped_column()
+    private:Mapped[bool] = mapped_column(default=True)
     room_id:Mapped[int] = mapped_column(ForeignKey('chat_rooms.id'))
     sender_id:Mapped[int] = mapped_column(ForeignKey('accounts.id'))
     created_on = mapped_column(DateTime(timezone=True), server_default=func.now())
